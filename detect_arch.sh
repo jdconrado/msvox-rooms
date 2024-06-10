3f97cb866cf92365e2ddf88a68828b4c6c4b6ade
Juan Conrado <jdconradop@gmail.com>
2024-06-09 22:50:37 -0500

    ci: set env vars for arm64 build

--- detect_arch.sh
+++ detect_arch.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 if [ "$(uname -m)" = "aarch64" ]; then
     echo "ARM architecture detected"
-    export MEDIASOUP_WORKER_BIN=./mediasoup-bins/arm64/mediasoup-worker
+    export MEDIASOUP_SKIP_WORKER_PREBUILT_DOWNLOAD="true"
+    export MEDIASOUP_WORKER_BIN=/app/mediasoup-bins/arm64/mediasoup-worker
     echo "Setting MEDIASOUP_WORKER_BIN to ./mediasoup-bins/arm64/mediasoup-worker"
 fi
