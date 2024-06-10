369a709b299c7a3210ac988a324409fd820da5e7
Juan Conrado <jdconradop@gmail.com>
2024-06-09 23:09:47 -0500

    ci: create docker ignore file

--- detect_arch.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-if [ "$(uname -m)" = "aarch64" ]; then
-    echo "ARM architecture detected"
-    export MEDIASOUP_SKIP_WORKER_PREBUILT_DOWNLOAD='true'
-    export MEDIASOUP_WORKER_BIN='/app/mediasoup-bins/arm64/mediasoup-worker'
-    echo "Setting MEDIASOUP_WORKER_BIN to /app/mediasoup-bins/arm64/mediasoup-worker"
-fi
