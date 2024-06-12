7a58032588af1da0a7abbe7ee9e1dd330c547b2f
Juan Conrado <jdconradop@gmail.com>
2024-06-11 23:08:45 -0500

    fix: update turn credential generation

--- src/infrastructure/mediasoup/services/ms-transport.adapter.ts
+++ src/infrastructure/mediasoup/services/ms-transport.adapter.ts
@@ -166,7 +166,10 @@ export class MSTransportAdapter implements IMSTransportAdapter {
     const { username, password } = this.generateTurnCredentials(transportId);
     return [
       {
-        urls: `turn:${APP_VARIABLES.MEDIASOUP_TURN_HOST}:5349?transport=udp`,
+        urls: [
+          `turn:${APP_VARIABLES.MEDIASOUP_TURN_HOST}:5349`,
+          'stun:${APP_VARIABLES.MEDIASOUP_TURN_HOST}:5349',
+        ],
         username,
         credential: password,
       },
